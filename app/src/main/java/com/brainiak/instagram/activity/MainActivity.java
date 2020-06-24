17276e46ec2eda21340ba9577ec5a7fefe976887
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-06-24 00:00:39 -0300

    create login

--- app/src/main/java/com/brainiak/instagram/activity/MainActivity.java
+++ app/src/main/java/com/brainiak/instagram/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.brainiak.instagram;
+package com.brainiak.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.brainiak.instagram.R;
 
 public class MainActivity extends AppCompatActivity {
 
