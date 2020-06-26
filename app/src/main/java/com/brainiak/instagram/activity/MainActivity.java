a1f06e81fa2427b8c1ca309732f0b583884d6f9e
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-06-26 18:52:37 -0300

    create toolbar

--- app/src/main/java/com/brainiak/instagram/activity/MainActivity.java
+++ app/src/main/java/com/brainiak/instagram/activity/MainActivity.java
@@ -2,15 +2,64 @@ package com.brainiak.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.brainiak.instagram.R;
+import com.brainiak.instagram.helper.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram");
+
+
+        //configuracao de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao()
+
+
+
     }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+            switch (item.getItemId()){
+                case R.id.menu_sair:
+                    deslogarUsuario();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    break;
+            }
+        return super.onOptionsItemSelected( item );
+    }
+
+    public void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ){
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu( menu );
+    }
+
+
+
 }
