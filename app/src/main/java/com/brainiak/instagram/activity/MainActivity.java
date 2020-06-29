fe4c36550f20fcd06b28deb4ca425b268cb07a4a
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-06-29 19:14:06 -0300

    create bottomNavigation

--- app/src/main/java/com/brainiak/instagram/activity/MainActivity.java
+++ app/src/main/java/com/brainiak/instagram/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.brainiak.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +13,15 @@ import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import com.brainiak.instagram.R;
+import com.brainiak.instagram.fragment.FeedFragment;
+import com.brainiak.instagram.fragment.PerfilFragment;
+import com.brainiak.instagram.fragment.PesquisaFragment;
+import com.brainiak.instagram.fragment.PostagemFragment;
 import com.brainiak.instagram.helper.ConfiguracaoFirebase;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao;
@@ -28,8 +38,61 @@ public class MainActivity extends AppCompatActivity {
         //configuracao de objetos
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
+        configuraBottomNavigationView();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.viewPage, new FeedFragment()).commit();
+
+    }
+
+    private void configuraBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationItemViewEx = findViewById(R.id.bottomNavigation);
 
 
+        //Fazer configurações iniciais do Bottom Navigation
+        bottomNavigationItemViewEx.enableAnimation(false);
+        bottomNavigationItemViewEx.enableItemShiftingMode(true);
+        bottomNavigationItemViewEx.enableShiftingMode(false);
+        bottomNavigationItemViewEx.setTextVisibility(true);
+
+        //Habilitar navegação
+        habilitarNavegacao( bottomNavigationItemViewEx);
+    }
+
+    /**
+     * Método responsável por tratar eventos de click na BottomNavigation
+     * @param viewEx
+     */
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+                switch (item.getItemId()){
+                    case R.id.ic_home :
+                        fragmentTransaction.replace(R.id.viewPage, new FeedFragment()).commit();
+                        return true;
+                    case R.id.ic_pesquisa :
+                        fragmentTransaction.replace(R.id.viewPage, new PesquisaFragment()).commit();
+                        return true;
+                    case R.id.ic_postagem :
+                        fragmentTransaction.replace(R.id.viewPage, new PostagemFragment()).commit();
+                        return true;
+                    case R.id.ic_perfil :
+                        fragmentTransaction.replace(R.id.viewPage, new PerfilFragment()).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
