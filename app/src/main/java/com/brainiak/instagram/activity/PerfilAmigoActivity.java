68729a632702d4b0c8f951a73ab9780176da44a9
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-07-15 18:03:41 -0300

    up

--- app/src/main/java/com/brainiak/instagram/activity/PerfilAmigoActivity.java
+++ app/src/main/java/com/brainiak/instagram/activity/PerfilAmigoActivity.java
@@ -1,18 +1,33 @@
 package com.brainiak.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.brainiak.instagram.R;
+import com.brainiak.instagram.helper.ConfiguracaoFirebase;
 import com.brainiak.instagram.model.Usuario;
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class PerfilAmigoActivity extends AppCompatActivity {
 
-private Usuario usuarioSelecionado;
+    private Usuario usuarioSelecionado;
     private Button buttonAcaoPerfil;
+    private CircleImageView imagePerfil;
+    private DatabaseReference usuariosRef;
+    private DatabaseReference usuarioAmigoRef;
+    private ValueEventListener valueEventListenerPerfilAmigo;
+    private TextView textPublicacao, textSeguidores, textSeguindo;
 
 
     @Override
@@ -20,9 +35,11 @@ private Usuario usuarioSelecionado;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_amigo);
 
+        //Configurações iniciais
+        usuariosRef = ConfiguracaoFirebase.getFirebase().child("usuarios");
+
         //Inicializar os componentes
-        buttonAcaoPerfil = findViewById(R.id.buttonAcaoPerfil);
-        buttonAcaoPerfil.setText("Seguir");
+        inicializarComponentes();
 
         //Configura toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -38,12 +55,68 @@ private Usuario usuarioSelecionado;
             //Configura o nome do usuario na toolbar
             getSupportActionBar().setTitle( usuarioSelecionado.getNome());
 
+            //Recupera foto do usuario
+            String caminhoFoto = usuarioSelecionado.getCaminhoFoto();
 
+            if(caminhoFoto != null) {
+                Uri url = Uri.parse( caminhoFoto );
+                Glide.with(PerfilAmigoActivity.this)
+                        .load( url )
+                        .into( imagePerfil );
+
+            }
         }
 
 
 
 
+    }
+
+    private void inicializarComponentes(){
+        buttonAcaoPerfil = findViewById(R.id.buttonAcaoPerfil);
+        buttonAcaoPerfil.setText("Seguir");
+        textPublicacao = findViewById(R.id.textPublicacao);
+        textSeguidores = findViewById(R.id.textSeguidores);
+        textSeguindo = findViewById(R.id.textSeguidos);
+        imagePerfil = findViewById(R.id.imageEditarPerfil);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarDadosPerfilAmigo();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void recuperarDadosPerfilAmigo(){
+
+        usuarioAmigoRef = usuariosRef.child( usuarioSelecionado.getId());
+        valueEventListenerPerfilAmigo = usuarioAmigoRef.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Usuario usuario = dataSnapshot.getValue( Usuario.class );
+
+                        String postagens = String.valueOf( usuario.getPostagens());
+                        String seguindo = String.valueOf( usuario.getSeguindo());
+                        String seguidores = String.valueOf( usuario.getSeguidores());
+
+                        //Configura valores recuperados
+                        textPublicacao.setText( postagens );
+                        textSeguidores.setText( seguidores );
+                        textSeguindo.setText( seguindo );
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
     }
 
     @Override
