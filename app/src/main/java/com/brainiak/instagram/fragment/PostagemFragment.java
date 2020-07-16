fb6dae27e461212deaed00a9367a3f90b8a26868
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-07-16 12:53:05 -0300

    update fragment_postagem

--- app/src/main/java/com/brainiak/instagram/fragment/PostagemFragment.java
+++ app/src/main/java/com/brainiak/instagram/fragment/PostagemFragment.java
@@ -1,13 +1,16 @@
 package com.brainiak.instagram.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.brainiak.instagram.R;
 
@@ -15,7 +18,8 @@ import com.brainiak.instagram.R;
  * A simple {@link Fragment} subclass.
  */
 public class PostagemFragment extends Fragment {
-
+    private Button buttonAbrirGaleria, buttonAbrirCamera;
+    private static final int SELECAO_CAMERA = 100, SELECAO_GALERIA = 200;
 
     public PostagemFragment() {
         // Required empty public constructor
@@ -26,7 +30,34 @@ public class PostagemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_postagem, container, false);
+        View view = inflater.inflate(R.layout.fragment_postagem, container, false);
+
+        //inicializar componentes
+        buttonAbrirCamera = view.findViewById(R.id.buttonAbrirCamera);
+        buttonAbrirGaleria = view.findViewById(R.id.buttonAbrirGaleria);
+
+        //Adiciona evento de clique o botao camera
+        buttonAbrirCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(i.resolveActivity(getActivity().getPackageManager()) != null ){
+                    startActivityForResult(i, SELECAO_CAMERA);
+                }
+            }
+        });
+
+        buttonAbrirGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getActivity().getPackageManager()) != null ){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
+        return view;
     }
 
 }
